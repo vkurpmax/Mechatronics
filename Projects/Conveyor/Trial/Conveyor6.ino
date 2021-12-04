903d5d47df211072e5a669ffaf99356e5e690a10
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2021-12-04 12:43:07 +0800

    Update Conveyor6.ino

--- Projects/Conveyor/Trial/Conveyor6.ino
+++ Projects/Conveyor/Trial/Conveyor6.ino
@@ -1,7 +1,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <Servo.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+Servo servo1;
+Servo servo2;
 
 int IN4 = 12;
 const int tombol = 8;
@@ -12,6 +16,9 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(8, INPUT);
   pinMode(tombol, INPUT_PULLUP);
+
+  servo1.attach(5);
+  servo2.attach(6);
   
   lcd.begin();
   lcd.setCursor(0,0);
@@ -33,21 +40,24 @@ void loop() {
 void pushbutton() {
   if (digitalRead(tombol) == LOW) {
     countertombol++;
-    delay(250);
+    delay(500);
     if (countertombol == 1) {
       digitalWrite(IN4, HIGH);
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("    CONVEYER    ");
+      lcd.print("    CONVEYOR    ");
       lcd.setCursor(0,1);
       lcd.print("       ON       ");
+      servo1.write(10);
     }
     if (countertombol == 2) {
       digitalWrite(IN4, LOW);
+      servo1.write(105);
+      servo2.write(93);
       countertombol = 0;
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("    CONVEYER    ");
+      lcd.print("    CONVEYOR    ");
       lcd.setCursor(0,1);
       lcd.print("      OFF");
       delay(3000);
