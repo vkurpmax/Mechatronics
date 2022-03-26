caa277f6b06da8b063679352eb7b2e418a22a260
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2022-03-26 12:19:05 +0800

    Create Door_Lock_System_02.ino

---
+++ General/RFID/Door_Lock_System_02.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define RELAY 4 //relay pin
+#define ACCESS_DELAY 2000
+#define DENIED_DELAY 1000
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();          // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  digitalWrite(RELAY, HIGH);
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  if (content.substring(1) == "29 D4 A0 C1" || "73 75 54 03") //TUKAR UID INI SESUAI RFIA TAG KALIAN
+  {
+    delay(500);
+    digitalWrite(RELAY, LOW);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY, HIGH);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(DENIED_DELAY);
+  }
+}
